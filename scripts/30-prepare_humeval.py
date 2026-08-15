a865442a5e166a04d6aefc3b4b295d471fd25faf
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-08-15 18:28:30 +0200

    prepare humeval
    Co-authored-by: Pinzhen Chen <15124011+PinzhenChen@users.noreply.github.com>

--- scripts/30-prepare_humeval.py
+++ scripts/30-prepare_humeval.py
@@ -12,7 +12,18 @@ submissions = [x for x in submissions if x["status"] == "accept"]
 
 campaigns_file = []
 
-MODELS = ["human", "Google Translate", "Gemini 3.5 Flash Lite", "GPT-5.4 Mini", "Qwen 3.7 Flash"]
+MODELS = ["human", "Google Translate", "Gemini 3.5 Flash Lite", "GPT-5.4 Mini", "Gemma 4"]
+STYLE_FORM = "<style>.form-container { max-width: 1000px !important; }</style>"
+STYLE_CESA = """
+<style>
+.output_scrollable { flex-direction: column; gap: 20px !important; }
+.output_item { display: block !important; }
+.output_src { width: calc(100% - 10px) !important; margin-bottom: 30px; }
+.output_block { width: 1000px; margin-left: auto; margin-right: auto;}
+.output_response { width: 400px !important; display: inline-block !important; }
+.slider_label_cESA { width: 200px; text-align: left; display: inline-block; }
+</style>
+"""
 
 for lang1, lang2 in [("Chinese (Simplified)", "English"), ("English", "Chinese (Simplified)")]:
     campaign_data = {
@@ -36,17 +47,42 @@ for lang1, lang2 in [("Chinese (Simplified)", "English"), ("English", "Chinese (
                 model: [x["translation"] for x in submission["translations"] if x["model"] == model][0]
                 for model in MODELS
                 if any(x["model"] == model for x in submission["translations"])
-            }
+            },
+            "instructions": STYLE_CESA,
         }
         doc_obj_rules = copy.deepcopy(doc_obj)
         doc_obj_rules["instructions"] = (
             "<div class='white-box' style='margin: -80px 10px 10px 0px; background-color: #e7e2cf;'>" +
             "Now also verify the translations by considering these rules. You may disagree with their necessity.<br><br>" +
             "<br>\n".join(["<b>Rule: </b>" + x["value"] for x in submission["verification_rules"]]) +
-            "<div>"
+            "<div>"+STYLE_CESA
         )
+        doc_obj_form = [
+            {
+                "text": "<b>Source text:</b> " + submission["source_text"] + STYLE_FORM,
+                "form": None,
+            },
+        ]
+
+        for rule in submission["verification_rules"]:
+            doc_obj_form.append({
+                "text": f"<br><br>Do you think the rule is realistic, or would it fail correct translations?<br><br><b>Rule:</b> {rule['value']}",
+                "form": "choices",
+                "choices": ["This rule is realistic and reasonable", "This rule is too strict", "Not sure"],
+            })
+        doc_obj_form.append({
+            "text": (
+                "<br><br>Are there translations that are incorrect but would pass all of these verifications at the same time?<br><br>"+
+                "<br>".join(["<b>Rule: </b>" + x["value"] for x in submission["verification_rules"]])
+            ),
+            "form": "choices",
+            "choices": ["These rules are fine as they cover most cases", "Some incorrect translations might pass through", "Not sure"],
+        })
+
+
         campaign_data["data"].append([doc_obj])
         campaign_data["data"].append([doc_obj_rules])
+        campaign_data["data"].append(doc_obj_form)
 
     campaign_data["data"] = [campaign_data["data"]]*2
     print(f"Campaign {lang1} -> {lang2} has {len(campaign_data['data'][0])} items")
